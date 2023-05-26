bfde3b136da7194f82fc48a32ec776a478440fe2
AndrewKPascual <95746939+AndrewKPascual@users.noreply.github.com>
2023-05-26 08:49:42 -0700

    Push
    a

--- seleniumscrape.py
+++ seleniumscrape.py
@@ -1,23 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import time
 
 # Set up the Selenium Chrome driver
-service = Service('/path/to/chromedriver')  # Replace with the path to your chromedriver executable
+service = Service(r'C:\Users\andre\Desktop\chromedriver\chromedriver.exe')  # Replace with the path to your chromedriver executable
 options = Options()
 options.add_argument('--headless')  # Run Chrome in headless mode, no GUI
 driver = webdriver.Chrome(service=service, options=options)
 
 # Load the website
-driver.get('https://langlabs.io/')
+driver.get('https://www.stack-ai.com/')  # Replace with the URL of the website you want to scrape
 
 # Wait for the dynamic content to load (you may need to adjust the wait time)
-driver.implicitly_wait(5)  # Waits for 5 seconds
+time.sleep(5)  # Adjust the delay as needed (in seconds)
 
-# Extract the desired information
-element = driver.find_element(By.XPATH, '//some/xpath')
-# Process or print the element's text or attribute as needed
+# Get the page source (HTML code) after the dynamic content has loaded
+page_source = driver.page_source
+
+# Write the page source to a file
+with open('page_source.html', 'w', encoding='utf-8') as file:
+    file.write(page_source)
 
 # Close the driver
 driver.quit()
